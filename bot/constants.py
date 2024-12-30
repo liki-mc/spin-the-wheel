27821a19e2c733390c8a056175e21d9b28bd8d46
liki-mc <liki.mc@outlook.com>
2024-12-30 23:59:19 +0100

    arcade doesnt easily run in docker

--- bot/constants.py
+++ bot/constants.py
@@ -1,3 +1,8 @@
+import pathlib
+PATH = pathlib.Path(__file__).parent.resolve()
+
+SECRETS = pathlib.Path(PATH.parent, "secrets")
+
 DISCORD_TOKEN: str
-with open("/run/secrets/discord_token", encoding="UTF-8") as secret:
+with open(pathlib.Path(SECRETS, "discord_token.txt"), encoding="UTF-8") as secret:
     DISCORD_TOKEN = secret.readline().rstrip("\n")
