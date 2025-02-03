b4322b9eef79381aeb5deecae7afe5837a1689e5
liki-mc <liki.mc@outlook.com>
2025-02-04 00:51:50 +0100

    added range option

--- bot/extensions/spin_the_wheel.py
+++ bot/extensions/spin_the_wheel.py
@@ -4,7 +4,10 @@ from discord.ext import commands
 from bot.bot import Bot
 from .arcade_wheel import spin, quickspin
 
+import re
 import shlex
+
+RANGE_REGEX = r"\s*(\d+) ?- ?(\d+)\s*"
 
 class SpinTheWheel(commands.Cog):
     def __init__(self, bot: Bot) -> None:
@@ -13,9 +16,8 @@ class SpinTheWheel(commands.Cog):
     @commands.command()
     async def ping(self, ctx: commands.Context):
         await ctx.send("Pong!")
-
-    @commands.command()
-    async def quickspin(self, ctx: commands.Context, *, args: str):
+    
+    def parse_args(self, args: str) -> list[str]:
         if "\n" in args:
             options = args.split("\n")
         elif ";" in args:
@@ -26,6 +28,19 @@ class SpinTheWheel(commands.Cog):
             options = args.split(",")
         else:
             options = args.split()
+    
+        for option in options:
+            if re.match(RANGE_REGEX, option):
+                start, end = map(int, re.match(RANGE_REGEX, option).groups())
+                index = options.index(option)
+                options.remove(option)
+                options[index:index] = map(str, range(start, end + 1))
+        
+        return options
+
+    @commands.command()
+    async def quickspin(self, ctx: commands.Context, *, args: str):
+        options = self.parse_args(args)
         
         avatar = ctx.author.display_avatar
         with open("data/avatar.png", "wb") as file:
@@ -41,16 +56,7 @@ class SpinTheWheel(commands.Cog):
     
     @commands.command()
     async def spin(self, ctx: commands.Context, *, args: str):
-        if "\n" in args:
-            options = args.split("\n")
-        elif ";" in args:
-            options = args.split(";")
-        elif "\"" in args:
-            options = shlex.split(args)
-        elif "," in args:
-            options = args.split(",")
-        else:
-            options = args.split()
+        options = self.parse_args(args)
         
         avatar = ctx.author.display_avatar
         with open("data/avatar.png", "wb") as file:
